b222b51da0724ea15cdfee7615bbfc438ee0b6e4
sterbb <webjrdivinagracia@gmail.com>
2024-07-10 22:07:37 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'SSP': ['Source Sans Pro', 'sans-serif']
+    },
+    fontSize:{
+      mammoth:"8rem"
+    },
+    extend: {},
+  },
+  
+  plugins: [],
+}
